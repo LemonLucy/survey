f9036976ccae55d62c4e0ac3b756845d8bd71552
eungyo <eungyo0965@gmail.com>
2024-10-27 09:52:13 +0100

    change route

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import HomePage from './pages/HomePage/HomePage';
 import SurveyForm from './pages/SurveyForm/SurveyForm';
 import ThankYouPage from './pages/ThankYou/ThankYou';
-import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
+import { HashRouter as Router,Routes,Route } from 'react-router-dom';
 
 function App() {
   return (
